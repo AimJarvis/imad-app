89bb48be33bfc3100903374600dba733019c416f
AimJarvis <umeshgp07@gmail.com>
2017-12-21 23:05:43 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="01css.css"/>
+<style>
+div{
+	width: 1200px;
+}
+
+#contact{
+	text-align:center;
+	border:1px solid red;background:white; position:absolute; top:46% ;left:5%; font-size:14px;padding:5px;
+}
+
+#contact:hover{
+	background:#FBD3F6;
+	transition:2s;
+}
+</style>
+</head>
+<body>
+<div id="header" style="width:1330;position:absolute; top:1% ;left:0.5%"> 
+<ul>
+<li><img src="images/logo.png"/></li>
+<li><a href='backhome.php'>Home</a></li>
+<li><a href="gamewindow.php">Games</a></li>
+<li><a href="leader.php">LeaderBoard</a></li>
+<li><a href="about.php">AboutUs</a></li>
+<li><a href="contact.php">ContactUs</a></li>
+<li><a href="logout.php">LogOut</a></li>
+</ul>
+</div>
+
+<div style =" width:99%;position:absolute; top:26% ;left:0.5%">
+<h1 align= "center" >DEVELOPERS</h1></br>
+</br></br>
+</div>
+<div id="contact">
+<BR>
+&nbsp;<b>UMESH PATOLE</b><BR>
+&nbsp;MOBILE: <i>+91-8007821924</i><BR>
+&nbsp;EMAIL: <i>umeshgp07@gmail.com</i><BR><BR>
+&nbsp;<b>SHANTANU SALIL</b><BR>
+&nbsp;MOBILE: <i>+91-8007821581</i><BR>
+&nbsp;EMAIL: <i>shantanu.salil123@gmail.com</i><BR><BR>
+&nbsp;<b>AMIT SINGH CHANDEL</b><BR>
+&nbsp;MOBILE: <i>+91-800782613</i><BR>
+&nbsp;EMAIL: <i>amsinghchandel@gmail.com</i><BR><BR>
+&nbsp;<b>PRAVENDRA SINGH </b><BR>
+&nbsp;MOBILE: <i>+91-8007865412</i><BR>
+&nbsp;EMAIL: <i>pravendra.singh1234@gmail.com</i>
+<BR><BR><br>
+</div>
+</body>
+</html>
